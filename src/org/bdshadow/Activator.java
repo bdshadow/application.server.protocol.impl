daa92239e0b06e556078c8d82ddb5257618ccf3c
Dmitrii Bocharov <dbocharo@redhat.com>
2018-05-18 15:28:07 +0200

    Initial commit

---
+++ src/org/bdshadow/Activator.java
@@ -0,0 +1,24 @@
+package org.bdshadow;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends Plugin {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Hello World!!");
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Goodbye World!!");
+	}
+
+}
